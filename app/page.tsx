51090f6d654ac90ace9289e4404ad09aff46f0c6
Mrudul Patel <84196599+mrudulpatel@users.noreply.github.com>
2025-10-18 05:11:25 +0000

    06 - feat: add Workflow model and migration, integrate Inngest for workflow creation
    - Added new Workflow model to schema.prisma with id and name fields.
    - Created migration file to set up Workflow table in the database.
    - Updated appRouter to include getWorkflows query and createWorkflow mutation using Inngest.
    - Added Inngest client to handle workflow events.

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,30 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { LogoutButton } from "@/features/auth/components/logout-button";
 import { requireAuth } from "@/lib/auth-utils";
+import { useTRPC } from "@/trpc/client";
 import { caller } from "@/trpc/server";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
 
-const Page = async () => {
-  await requireAuth();
+const Page = () => {
+  // await requireAuth();
+  const trpc = useTRPC();
+  const queryClient = useQueryClient();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
 
-  const data = await caller.getUsers();
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      toast.success("Workflow creation queued");
+    }
+  }));
   return (
-    <div className="min-h-screen min-w-screen flex items-center justify-center">
-      {JSON.stringify(data)}
+    <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
+      protected server component
+      <div>{JSON.stringify(data, null, 2)}</div>
+
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>Create Workflow</Button>
+      
       <LogoutButton />
     </div>
   );
--- trpc/routers/_app.ts
+++ trpc/routers/_app.ts
@@ -1,12 +1,18 @@
 import prisma from "@/lib/db";
-import { protectedProcedure, createTRPCRouter } from "../init";
+import { createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    console.log({ userId: ctx.auth.user.id });
-    return prisma.user.findMany({
-      where: { id: ctx.auth.user.id }
-    });
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
   }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "test@gmail.com"
+      }
+    });
+  })
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
